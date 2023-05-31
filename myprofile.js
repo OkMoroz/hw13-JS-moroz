3e208aa6685ecb1d238d74fdf0ad5c210ef3e481
Oksana Moroz <k.moroz.mail@gmail.com>
2023-06-01 00:32:00 +0300

    final

---
+++ myprofile.js
@@ -0,0 +1,8 @@
+const windowWidth = screen.width;
+const windowHeight = screen.height;
+console.log("Screen width is:", windowWidth);
+console.log("Screen height is:", windowHeight);
+
+function goBack() {
+  window.history.back();
+}
